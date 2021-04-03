d71f4473916f96093933f0aade48a3d3608af5d0
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-04-04 01:19:04 +0300

    add signal handlers

--- kube_pyfuse/__main__.py
+++ kube_pyfuse/__main__.py
@@ -1,13 +1,22 @@
+import sys
+import signal
+
 import fuse
 
 from .fs import KubeFS
 
 
-# TODO: add signal handlers (SIGTERM, SIGINT)
 def main():
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
+
     server = KubeFS(version="%prog " + fuse.__version__, dash_s_do='setsingle')
     server.parse(errex=1)
     server.main()
+
+
+def shutdown(signum, frame):
+    sys.exit(0)
 
 
 if __name__ == '__main__':
